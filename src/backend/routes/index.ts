9e32b74db04b5cab519794c4679f7b2d7e313498
Kajetan Krawczyk <kaitek666@gmail.com>
2023-04-15 10:01:31 +0100

    baza danych

--- src/backend/routes/index.ts
+++ src/backend/routes/index.ts
@@ -1,13 +1,15 @@
-import express from 'express'
-import { getStatus } from './status/get.status'
-const router = express.Router()
+import express from 'express';
+import { getStatus } from './status/get.status';
+import { postUser } from './users/post.user';
+const router = express.Router();
 // middleware
 router.use((req, res, next) => {
-    console.log('Time: ', Date.now())
-    next()
-})
+    console.log('Time: ', Date.now());
+    next();
+});
 // home page route
 router.get('/', (req, res) => {
-    res.send('Example home page')
-})
-export default router
+    res.send('Example home page');
+});
+router.post('/api/user', postUser);
+export default router;
